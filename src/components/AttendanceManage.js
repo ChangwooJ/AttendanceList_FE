3a12d492661faeb9347843c7388839698e02ac6d
ChangwooJ <wjdckddn99@naver.com>
2024-12-12 13:18:13 +0900

    이미지 편집x

--- src/components/AttendanceManage.js
+++ src/components/AttendanceManage.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Fuse from "fuse.js";
-import Cropper from "react-cropper";
-import "cropperjs/dist/cropper.css";
 
 import "../css/AttendanceManage.css";
 
@@ -12,24 +10,20 @@ const AttendanceManage = () => {
     const [bestMatch, setBestMatch] = useState("");
     const [absentList, setAbsentList] = useState([]);
     const [fileExist, setFileExist] = useState(false);
-    const [dots, setDots] = useState("");   //점의 개수 상태
-    const [image, setImage] = useState(null);   //원본 이미지
-    const [croppedImage, setCroppedImage] = useState(null);     //자른 이미지
-    const cropperRef = useRef(null);
-
-
+    const [dots, setDots] = useState(""); // 점의 개수 상태
+  
     useEffect(() => {
-        const interval = setInterval(() => {
-            setDots((prev) => (prev.length < 3 ? prev + "." : ".")); // 점 3개까지 증가 후 초기화
-        }, 500); // 0.5초 간격으로 업데이트
-
-        return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 정리
+      const interval = setInterval(() => {
+        setDots((prev) => (prev.length < 3 ? prev + "." : ".")); // 점 3개까지 증가 후 초기화
+      }, 500); // 0.5초 간격으로 업데이트
+  
+      return () => clearInterval(interval); // 컴포넌트 언마운트 시 인터벌 정리
     }, []);
 
     const fetchUserList = async () => {
         try {
             const response = await axios.get("https://goyang0360.o-r.kr/api/userlist");
-            setUserList(response.data);
+            setUserList(response.data); 
         } catch (error) {
             console.error("유저 목록 불러오기 실패:", error);
         }
@@ -39,7 +33,7 @@ const AttendanceManage = () => {
         fetchUserList();
     }, []);
 
-    const handleUploadImg = (event) => {
+    const handleUploadImg = async (event) => {
         const file = event.target.files[0];
         if (!file) {
             alert("이미지를 업로드해주세요.");
@@ -47,28 +41,10 @@ const AttendanceManage = () => {
         }
 
         setFileExist(true);
-        setImage(URL.createObjectURL(file)); // 이미지 미리보기 설정
-    };
 
-    // 이미지를 자른 후 croppedImage 상태 업데이트
-    const getCroppedImage = () => {
-        if (cropperRef.current) {
-            cropperRef.current.cropper.getCroppedCanvas().toBlob((blob) => {
-                // Blob을 FormData에 추가합니다.
-                const formData = new FormData();
-                formData.append("image", blob, "image"); // 파일명 지정
-    
-                setCroppedImage(URL.createObjectURL(blob)); // 자른 이미지를 미리보기로 설정
-                setImage(null);
-    
-                // 서버로 전송하는 부분을 추가합니다.
-                handleUploadCroppedImg(formData);
-            }, 'image/png');
-        }
-    };
+        const formData = new FormData();
+        formData.append("image", file);
 
-    // 서버로 이미지를 전송하는 함수
-    const handleUploadCroppedImg = async (formData) => {
         try {
             const response = await axios.post("https://goyang0360.o-r.kr/ocr", formData, {
                 headers: {
@@ -77,6 +53,7 @@ const AttendanceManage = () => {
             });
 
             const scanlist = response.data.text.replace(/[.,]/g, "").split(/\s+/).filter(Boolean);
+
             setResult(scanlist || "텍스트를 찾을 수 없습니다.");
             findSimilarUser(scanlist);
         } catch (error) {
@@ -85,7 +62,7 @@ const AttendanceManage = () => {
         }
     };
 
-    const findSimilarUser = (ocrText) => {
+    const findSimilarUser = (ocrText) => {        
         const options = {
             includeScore: true,
             threshold: 0.6,
@@ -109,7 +86,7 @@ const AttendanceManage = () => {
                 };
             }
         });
-
+    
         setBestMatch(results);
         attendanceProcess(results);
     };
@@ -117,7 +94,7 @@ const AttendanceManage = () => {
     const attendanceProcess = (results) => {
         const filteredResults = results.filter((result) => result.score !== null);
 
-        const absentUser = userList.filter((user) =>
+        const absentUser = userList.filter((user) => 
             filteredResults.some((result) => result.bestMatch === user.username)
         );
 
@@ -126,40 +103,16 @@ const AttendanceManage = () => {
         const updatedUserList = userList.filter(
             (user) => !filteredResults.some((res) => res.bestMatch === user.username)
         );
-
+    
         setUserList(updatedUserList);   //출석자 명단
     }
 
     return (
         <React.Fragment>
-            {!result && !croppedImage && !image && (
+            {!result && (
                 <div className="upload_bt">
                     <input type="file" accept="image/*" onChange={handleUploadImg} />
-                    {fileExist && !result && (
-                        <p className="loading">인식중입니다. 잠시만 기다려주세요{dots}</p>
-                    )}
-                </div>
-            )}
-
-            {image && (
-                <div className="image_cropper">
-                    <Cropper
-                        src={image}
-                        ref={cropperRef}
-                        style={{ width: "100%", height: "auto" }}
-                        guides={false} // 자르기 가이드선 비활성화
-                        cropBoxResizable={true}
-                        cropBoxMovable={true}
-                    />
-                    <button className="cropper_bt" onClick={() => getCroppedImage()}>자르기</button>
-                </div>
-            )}
-
-            {croppedImage && (
-                <div className="cropped_preview">
-                    <img src={croppedImage} alt="Cropped preview"
-                        style={{ width: "100%", height: "auto" }} />
-                    <button className="cropper_bt" onClick={() => handleUploadCroppedImg()}>완료</button>
+                    {fileExist && !result && (<p className="loading">인식중입니다. 잠시만 기다려주세요{dots}</p>)}
                 </div>
             )}
             {result && <div className="scanList">{result.map((res, index) => (
@@ -169,7 +122,7 @@ const AttendanceManage = () => {
             ))}</div>}
             {bestMatch && <div className="matchingList">→ {bestMatch.map((bm, index) => (
                 <>
-                    {!bm.score &&
+                    {!bm.score && 
                         <p key={index} className="matchingUser">
                             {bm.bestMatch}
                         </p>}
